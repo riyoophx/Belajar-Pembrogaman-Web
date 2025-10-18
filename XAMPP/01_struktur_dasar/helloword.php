698a8ca3431ed71114332b52b5f877277cbec661
riyoophx <riohebat08@gmail.com>
2025-10-18 18:49:28 +0800

    PW6_PHP_01

---
+++ XAMPP/01_struktur_dasar/helloword.php
@@ -0,0 +1,10 @@
+<?php
+$teks = "Hello World!";
+$sebuah_bilangan = 4;
+$bilanganYangLain = 8.567;
+$teks2 = $teks;
+echo $teks;
+echo "<br />" . $sebuah_bilangan;
+echo "<br />Isi dari variable \$bilanganYangLain : " . $bilanganYangLain;
+echo "<br />Isi dari variable \$teks2 adalah : " . $teks2;
+?>
---
+++ XAMPP/01_struktur_dasar/test4.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <?php
+        echo "<title>Halaman PHP pertama</title>";
+        ?>
+    </head>
+    <body>
+        <h1>Script PHP dalam (X)HTML</h1>
+
+        <?php
+        echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+        echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+        ?>
+
+        
+        <p>Ini paragraph yang dibuat tanpa script PHP</p>
+        <p>Ini paragraph yang dibuat tanpa script PHP</p>
+
+        <?php
+        echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+        echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+        ?>
+    </body>
+</html>
